6d795445d1efa6e162b928aea0ea992f83bc4100
RodrigoSerratoGomez <s_o_a_d@hotmail.com>
2023-02-02 18:48:55 -0500

    Commit 02

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const ejs = require("ejs");
+const mongoose = require('mongoose');
 const _ = require('lodash');
-
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(
+  "mongodb+srv://rodrigo-admin:TzdrSNyJgPS0VGwH@cluster0.yqkcimk.mongodb.net/blogDB",
+  { useNewUrlParser: true },
+  (err) => { err ? console.log(err) : console.log("MongDB Successfull Connect"); }
+);
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -14,10 +20,16 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", (req, res) => {
-  res.render("home", { startingContent: homeStartingContent, posts: posts });
+  Post.find({}, (error, posts) => {
+    res.render("home", { startingContent: homeStartingContent, posts: posts });
+  })
 });
 
 app.get("/about", (req, res) => {
@@ -33,23 +45,21 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
+  });
+  post.save();
   res.redirect("/");
 });
 
-app.get("/post/:postName", (req, res) => {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(post => {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) res.render("post", {title: post.title, content: post.content});
+app.get("/post/:postId", (req, res) => {
+  const requestedTitle = req.params.postId;
+  Post.findById(requestedTitle, (error, post) => {
+    res.render("post", { title: post.title, content: post.content })
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(port || 3000, () => {
+    console.log(`Puerto aperturado en la ruta ${port}`);
 });
